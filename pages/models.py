1c82af459411b147221a4bf0e231a06f79f0933f
hamadk24 <hamadalkhaled24@gmail.com>
2020-01-14 19:26:42 +0300

    MiniProject

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now_add=True)
+
+	def get_absolute_url(self):
+		return "/page/%i/" % self.id
